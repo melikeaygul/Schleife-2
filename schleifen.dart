375fa67f49d075a674efd3d7c37a0546b3443071
melikeaygul <melikeaygul0810@gmail.com>
2025-03-10 16:30:36 +0100

    letzte anpassung

--- schleifen.dart
+++ schleifen.dart
@@ -1,12 +1,12 @@
 void main() {
-  for (int i = 0; i <= 10; i++) {
+  for (int i = 0; i <= 9; i++) {
     print("Kleine Zahl: $i");
   }
-  for (int i = 11; i <= 60; i++) {
+  for (int i = 10; i <= 59; i++) {
     print("Mittlere Zahl: $i");
   }
 
-  for (int i = 61; i <= 100; i++) {
+  for (int i = 60; i <= 100; i++) {
     print("Große Zahl: $i");
   }
 }
