c4671eae3b79baaec95eaeb33be2862180df6e67
melikeaygul <melikeaygul0810@gmail.com>
2025-03-10 12:23:11 +0100

    first.try

---
+++ schleifen.dart
@@ -0,0 +1,13 @@
+void main() {
+  for (int i = 0; i <= 10; i++) {
+    print("Kleine Zahl: $i");
+  }
+  for (int i = 11; i <= 60; i++) {
+    print("Mittlere Zahl: $i");
+  }
+
+  for (int i = 61; i <= 100; i++) {
+    String grosse = "";
+    print("Große Zahl: $i");
+  }
+}
